6d8a3ec094d3a8a9fc81b877ae6cca2bcbcd7ff3
nellywalker <theodoreratiaharivoavy.eni@gmail.com>
2023-03-20 15:09:07 +0100

    Change HOST:127.0.0.1 in setting.py

--- gestion_bancaires/gestion_bancaires/settings.py
+++ gestion_bancaires/gestion_bancaires/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "NAME": "bd_gestion_bancaires",
         "USER": "bda",
         "PASSWORD": "password",
-        "HOST": "localhost",
+        "HOST": "127.0.0.1",
         "PORT": "3306",
         "OPTIONS": {
             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
